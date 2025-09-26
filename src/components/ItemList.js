53b90752f3fb35c45a711c68e432903350a9385c
sakshi <sakshi0981456@gmail.com>
2025-09-26 22:41:31 +0530

    Fix RestaurantCategory toggle and update ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,17 +1,36 @@
+import { CDN_URL } from "../utils/constants.js";
+
 const ItemList = ({ items }) => {
-    
   return (
-    <ul>
+    <div>
       {items?.map((item) => (
-        <li
-          key={item?.card?.info?.id}
-          className="w-6/12 bg-gray-200 mx-auto my-6 font-medium p-6 text-lg"
-        >
-          {item?.card?.info?.name} – ₹
-          {(item?.card?.info?.price || item?.card?.info?.defaultPrice) / 100}
-        </li>
+        <div 
+        key={item?.card?.info?.id}
+        className="w-6/12 bg-gray-200 mx-auto my-6 font-medium p-6 text-lg border-gray-200 border-b-2 flex text-left"
+       >
+          
+        <div className=" w-9/12 ">
+        <div className="py-2">
+            <span>{item?.card?.info?.name}</span> 
+             <span> 
+              – ₹
+              {(item?.card?.info?.price || item?.card?.info?.defaultPrice) / 100}
+              </span>
+        </div>
+        <p className="text-xs"> {item.card.info.description}</p>
+        </div>
+        <div className="w-3/12 p-4">
+        <div className="absolute">
+          <button className ="p-2 mx-15 rounded-lg  bg-white shadow-lg m-auto">
+            Add + 
+          </button>
+          </div>
+          <img src= {CDN_URL + item.card.info.imageId } className="w-full" /> 
+        </div>
+        </div>
+        
       ))}
-    </ul>
+    </div>
     
   );
 };
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,12 +1,26 @@
+import React, { useState } from "react";
 import ItemList from "./ItemList.js";
 
-const RestaurantCategory = ({ category }) => {
+const RestaurantCategory = ({ category}) => {
+  const [showItems, setShowItems] = useState(false);
+  
+
+    const handleClick =() => {
+      console.log("Clicked");
+      setShowItems(!showItems);
+    }
   return (
     <div>
-      <h2 className="w-6/12 bg-gray-200 mx-auto my-6 font-bold p-6 text-xl ">
-        {category.title}
-      </h2>
-      <ItemList items={category.itemCards} />
+      <div className="w-6/12 bg-gray-200 mx-auto my-6 font-bold p-4 shadow-lg text-xl ">
+       <div className="flex-justify-between cursor-pointer" onClick ={handleClick}>
+          <span className="text-lg">
+            {category.title} {category.itemCards.length} 
+          </span> 
+            <span> 🔽 </span>
+          </div>
+        </div>
+      
+      { showItems && <ItemList items={category.itemCards} />}
     </div>
   );
 };
